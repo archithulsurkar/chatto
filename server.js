bd287a3539395f9b8b2912a3e3db303ccbc9820f
Archit Hulsurkar <archit.3572@gmail.com>
2025-12-11 11:15:15 -0800

    Add username login, online users sidebar, and join/leave notifications

--- server.js
+++ server.js
@@ -8,19 +8,51 @@ const io = new Server(server);
 // Serve static files from 'public' folder
 app.use(express.static('public'));
 
+// Store connected users: { socketId: username }
+const users = {};
+
 // Handle socket connections
 io.on('connection', (socket) => {
-    console.log('A user connected');
-    
+    console.log('A user connected:', socket.id);
+
+    // Handle user joining with a username
+    socket.on('user join', (username) => {
+        users[socket.id] = username;
+        console.log(`${username} joined the chat`);
+        
+        // Notify everyone that a new user joined
+        io.emit('user joined', {
+            username: username,
+            users: Object.values(users)
+        });
+    });
+
     // Handle disconnection
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        const username = users[socket.id];
+        if (username) {
+            delete users[socket.id];
+            console.log(`${username} left the chat`);
+            
+            // Notify everyone that user left
+            io.emit('user left', {
+                username: username,
+                users: Object.values(users)
+            });
+        }
     });
-    
+
     // Handle incoming chat messages
     socket.on('chat message', (msg) => {
-        // Broadcast the message to all connected clients
-        io.emit('chat message', msg);
+        const username = users[socket.id];
+        if (username) {
+            // Broadcast message with username and timestamp
+            io.emit('chat message', {
+                username: username,
+                message: msg,
+                timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            });
+        }
     });
 });
 
